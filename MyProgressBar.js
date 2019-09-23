efc7559ed710b462e92decd38224784baa7efa0f
pachesnyi <vanya.pachesnyi@gmail.com>
2019-09-23 19:10:57 +0300

    ADD: generate markup of progress bar dynamically

--- MyProgressBar.js
+++ MyProgressBar.js
@@ -8,6 +8,15 @@
             throw new Error('element should be HTMLElement');
         }
         this._element = element;
+        this._line = element.querySelector('.line');
+
+        if(!this._line) {
+            this._line = document.createElement('div');
+            this._line.classList.add('line');
+            this._element.append(this._line);
+        }
+
+
 
         if (element.hasAttribute("data-to")) {
             this.setValue(element.getAttribute('data-to'));
@@ -21,12 +30,16 @@
 
     // SetValue Method
     MyProgressBar.prototype.setValue = function (value) {
+        var self = this;
         value = parseInt(value, 10);
         if (value > 100 || value < 0) {
             throw new Error('value unexpected value');
         }
-        var line = this._element.querySelector('.line');
-        line.style.width = value + '%';
+        setTimeout(function () {
+            self._line.style.width = value + '%';
+        }, 0)
+
+
     };
 
     if(!window.MyProgressBar) {
--- main.js
+++
@@ -1,9 +0,0 @@
-// (function () {
-	// document.addEventListener( 'DOMContentLoaded', function () {
-    // var bar= new MyProgressBar(document.querySelector('.my-bar'));
-    //  window.__testbar=bar; 
-    // });
-// })();
-
-
- 
